f6e691202c4dba8b2ae6887c75f8eb3bd8694360
lifeishell21 <94784466+lifeishell21@users.noreply.github.com>
2023-12-13 09:21:49 +0000

    variables details

---
+++ Basicsjs/variables1.js
@@ -0,0 +1,21 @@
+const id =50;
+let email ="abhi@gmail.com";
+
+var password ="2333";
+
+City ="Motihari";
+
+let roomno;
+
+// id =51;    not allowed 
+/*
+  not use var because of block scope and functional scope..............
+*/
+
+email = "abhishek123@gmail.com";
+password ="123";
+City ="Delhi";
+console.table([id,email,password,City,roomno])
+console.log(id);
+
+
