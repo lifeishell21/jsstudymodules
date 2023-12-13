7440bf4609932754dd0f8bc05970ea2c6632433a
lifeishell21 <94784466+lifeishell21@users.noreply.github.com>
2023-12-13 11:23:24 +0000

    data types summary

---
+++ Basicsjs/Datatypes2.js
@@ -0,0 +1,39 @@
+//primitive
+
+// 7 types  String  , number , Boolean , Null , undefined , Symbol, BigInt 
+
+
+
+const score =100;
+
+
+
+const score2 = false;
+
+const logIn =false;
+
+const outsideTemp = null;
+let userEmail;  // let userEmail = undefined  same
+
+const id =Symbol('123');
+const id2 = Symbol('123');
+
+console.log(id === id2);
+
+const bigNumber =7676686867767n;
+
+// non primitive or reference tpe
+
+//Array , objects , functions...........
+const superhero =["ironman","batman","Thor","Antman","Superman"];
+
+
+const myfunction = function(){
+
+    console.log("Hey how are you!");
+}
+
+// use typeof to know the data type of any variables.............
+
+
+
