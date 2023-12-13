6ed6a0e02c6485b11b4cbbfd1cbbbfe4646eddd1
lifeishell21 <94784466+lifeishell21@users.noreply.github.com>
2023-12-13 10:39:12 +0000

    operations basics

---
+++ Basicsjs/Operation.js
@@ -0,0 +1,25 @@
+let value = 10;
+let negative =-value;
+console.log(negative);
+
+console.log(10+10);
+console.log(10*10);
+
+let str1 ="Hi";
+let str2 = "Abhi";
+console.log(str1+" "+str2);
+
+console.log("1"+ 2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log(+true);
+console.log(+"");
+
+
+let num1 =10;
+num1++;
+console.log(num1);
+
+// if in first string then all became string
+
