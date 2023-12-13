6d4166f0d4482a023e7ee20e9190cf117389d9cd
lifeishell21 <94784466+lifeishell21@users.noreply.github.com>
2023-12-13 09:38:28 +0000

    Basics Datatypes

---
+++ Basicsjs/Datatypes.js
@@ -0,0 +1,19 @@
+//"use strict" // to use all in newer version
+
+//alert("Why")   we are using node js , not browser...
+
+let name =" Abhi";
+let age = 22;
+let loggedIn = false;
+
+// number
+// bigint
+//string =""
+//boolean = true or false;
+// null >=  standalone value
+// symbol basically use in react for uniquness;
+// object
+
+console.log(typeof age);
+console.log(typeof null);  // object
+console.log(typeof undefined);
