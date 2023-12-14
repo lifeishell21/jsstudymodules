559f539981e7876f1ffdff15759030870835da8f
lifeishell21 <94784466+lifeishell21@users.noreply.github.com>
2023-12-14 07:03:11 +0000

    mathsandnumbers

---
+++ Basicsjs/NumbersandMaths.js
@@ -0,0 +1,79 @@
+const score =100;
+
+console.log(score);
+
+const balance = new Number(400);
+
+console.log(balance);
+
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2));
+
+const other = 23.8784;
+
+console.log(other.toPrecision(3));
+
+const num1 =90000000;
+console.log(num1.toLocaleString());
+
+console.log(num1.toLocaleString('en-IN'));
+
+
+//*******************MAthsssss */
+
+console.log(Math);
+
+console.log(Math.abs(-100));   //neg into positive
+
+console.log(Math.round(4.6));
+
+console.log(Math.ceil(4.6));
+
+console.log(Math.floor(4.9));
+
+console.log(Math.min(5,7,88,1,4,0));
+
+console.log(Math.max(5,7,88,1,4,0));
+
+console.log(Math.random());
+
+console.log(Math.floor(Math.random()*10 ) +1);
+
+const min =10;
+const max =20;
+
+console.log(Math.floor(Math.random()*(max-min +1)) + min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
